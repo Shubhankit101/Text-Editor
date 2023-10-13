fd8f42587ff3e03c5a2255cd89c06c42d2b4bc9a
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-10-13 15:00:37 +0530

    added theme change feature

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Get references to the editor and save button
+const main = document.querySelector('.main');
+const content = document.querySelector('.content');
 const editor = document.getElementById('editor');
 const saveButton = document.getElementById('save-button');
 const resetButton = document.getElementById('clear-button');
+const themeButton = document.getElementById('theme-change');
+
+let lightTheme = true;
+
+themeButton.addEventListener('click', function(){
+    if(lightTheme){
+        main.classList.add('dark');
+        content.classList.remove('lightContent');
+        content.classList.add('darkContent');
+        lightTheme = false;
+    }
+    else{
+        main.classList.remove('dark');
+        content.classList.add('lightContent');
+        content.classList.remove('darkContent');
+        lightTheme = true;
+    }
+});
+
 
 // Add an event listener to the save button
 saveButton.addEventListener('click', () => {
@@ -10,6 +31,7 @@ saveButton.addEventListener('click', () => {
     // Example using local storage:
     localStorage.setItem('editorContent', content);
 });
+
 
 resetButton.addEventListener('click', function(){
 localStorage.clear();
